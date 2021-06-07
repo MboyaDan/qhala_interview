c2098b69a7f8c6657f4a3c49c75b84df6b01c9f9
MboyaDan <onyangoroylex@gmail.com>
2021-06-07 13:34:08 -0700

    Completed Login routes

--- src/main/java/com/aim/app/configs/MyUserDetails.java
+++ src/main/java/com/aim/app/configs/MyUserDetails.java
@@ -1,43 +1,55 @@
 package com.aim.app.configs;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 public class MyUserDetails implements UserDetails {
+
+    public String userName;
+
+    public MyUserDetails(String userName){
+        this.userName = userName;
+
+    }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    public MyUserDetails() {
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return "pass";
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return userName;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
